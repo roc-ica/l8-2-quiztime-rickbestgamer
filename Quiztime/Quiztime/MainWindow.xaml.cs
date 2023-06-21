71619d6117e9f55cc458e42ee746a8e3d713dbe6
rickbestgamer <95346478+rickbestgamer@users.noreply.github.com>
2023-06-21 22:52:45 +0200

    pc

--- Quiztime/Quiztime/MainWindow.xaml.cs
+++ Quiztime/Quiztime/MainWindow.xaml.cs
@@ -26,39 +26,10 @@ namespace Quiztime
     {
         public MainWindow()
         {
-            CreateDB();
             InitializeComponent();
             mydata md = new mydata();
             md.GetDBData();
             QuizGrid.ItemsSource = ExcistingQuiz.excistingQuizzes;
-            
-        }
-
-        static void CreateDB()
-        {
-            string connectionString = "Data Source=localhost;Integrated Security=SSPI;database=master;";
-            string cmdText = "create database MYTESTDB";
-            SqlConnection sqlConnection = new SqlConnection(connectionString);
-            if (sqlConnection.State != ConnectionState.Open)
-            {
-                try
-                {
-                    Console.WriteLine("connection is closed");
-                    sqlConnection.Open();
-                    Console.WriteLine("connection is open");
-                    SqlCommand sqlCommand = new SqlCommand(cmdText);
-                    sqlCommand.ExecuteNonQuery();
-                    Console.WriteLine("database created");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                finally
-                {
-                sqlConnection.Close();
-                }
-            }
         }
 
          private void OpenQuiz(object sender, RoutedEventArgs e)
